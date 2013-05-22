f9066efc2162790a453a4d44d3c57b8cfec2f58e
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-05-22 19:34:25 +0000

    - Cleaned up @category, @package, and @subpackage for phpDoc generation

--- libraries/Cron.php
+++ libraries/Cron.php
@@ -3,9 +3,9 @@
 /**
  * Cron class.
  *
- * @category   Apps
- * @package    Tasks
- * @subpackage Libraries
+ * @category   apps
+ * @package    tasks
+ * @subpackage libraries
  * @author     ClearFoundation <developer@clearfoundation.com>
  * @copyright  2003-2011 ClearFoundation
  * @license    http://www.gnu.org/copyleft/lgpl.html GNU Lesser General Public License version 3 or later
@@ -81,9 +81,9 @@ clearos_load_library('tasks/Cron_Configlet_Not_Found_Exception');
 /**
  * Cron class.
  *
- * @category   Apps
- * @package    Tasks
- * @subpackage Libraries
+ * @category   apps
+ * @package    tasks
+ * @subpackage libraries
  * @author     ClearFoundation <developer@clearfoundation.com>
  * @copyright  2003-2011 ClearFoundation
  * @license    http://www.gnu.org/copyleft/lgpl.html GNU Lesser General Public License version 3 or later
--- libraries/Cron_Configlet_Not_Found_Exception.php
+++ libraries/Cron_Configlet_Not_Found_Exception.php
@@ -3,9 +3,9 @@
 /**
  * Cron configlet not found class.
  *
- * @category   ClearOS
- * @package    Tasks
- * @subpackage Exceptions
+ * @category   apps
+ * @package    tasks
+ * @subpackage libraries
  * @author     ClearFoundation <developer@clearfoundation.com>
  * @copyright  2003-2011 ClearFoundation
  * @license    http://www.gnu.org/copyleft/lgpl.html GNU Lesser General Public License version 3 or later
@@ -63,9 +63,9 @@ clearos_load_library('base/Engine_Exception');
 /**
  * Cron configlet not found class.
  *
- * @category   Apps
- * @package    Tasks
- * @subpackage Exceptions
+ * @category   apps
+ * @package    tasks
+ * @subpackage libraries
  * @author     ClearFoundation <developer@clearfoundation.com>
  * @copyright  2003-2011 ClearFoundation
  * @license    http://www.gnu.org/copyleft/lgpl.html GNU Lesser General Public License version 3 or later
