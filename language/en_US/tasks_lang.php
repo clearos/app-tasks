0713e2bc12908d101e16f51b1d75080e5fe0013f
clearostranslations <translations@clearfoundation.com>
2014-06-14 12:09:43 -0400

    Tidied up translation file

--- language/en_US/tasks_lang.php
+++ language/en_US/tasks_lang.php
@@ -2,7 +2,4 @@
 
 $lang['tasks_app_name'] = 'Task Scheduler';
 $lang['tasks_app_description'] = 'The Task Scheduler can be used to run batch jobs on your system at regular time intervals.';
-$lang['tasks_scheduler'] = 'Scheduler';
-$lang['tasks_task_scheduler'] = 'Task Scheduler';
-$lang['tasks_app_summary'] = 'Task summary';
 $lang['tasks_schedule_configuration_already_exists'] = 'Schedule configuration already exists.';
