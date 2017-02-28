02fabe6c82bde17dd539d2ff368106030c43f32f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:42:57 +0000

    Add new files and directories and commit changes to local repository

--- language/sl/tasks_lang.php
+++ language/sl/tasks_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['tasks_app_description'] = 'The Task Scheduler can be used to run batch jobs on your system at regular time intervals.';
+$lang['tasks_app_description'] = 'Task Scheduler se lahko uporablja za zagon paketna opravila na vašem sistemu v rednih časovnih intervalih.';
 $lang['tasks_app_name'] = 'Task Scheduler';
-$lang['tasks_schedule_configuration_already_exists'] = 'Schedule configuration already exists.';
+$lang['tasks_schedule_configuration_already_exists'] = 'Konfiguracija urnik že obstaja.';
