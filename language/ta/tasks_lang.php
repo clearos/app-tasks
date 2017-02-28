32a02a410ea5e6efa0469ebecb9c64bccd69bce7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:43:53 +0000

    Add new files and directories and commit changes to local repository

--- language/ta/tasks_lang.php
+++ language/ta/tasks_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['tasks_app_description'] = 'The Task Scheduler can be used to run batch jobs on your system at regular time intervals.';
-$lang['tasks_app_name'] = 'Task Scheduler';
-$lang['tasks_schedule_configuration_already_exists'] = 'Schedule configuration already exists.';
+$lang['tasks_app_description'] = 'பணி திட்டமிடுதல் வழக்கமான கால இடைவெளியில் உங்கள் கணினியில் தொகுதி வேலைகள் இயக்க பயன்படுத்த முடியும்.';
+$lang['tasks_app_name'] = 'பணி திட்டமிடுதல்';
+$lang['tasks_schedule_configuration_already_exists'] = 'அட்டவணை கட்டமைப்பு ஏற்கனவே உள்ளது.';
