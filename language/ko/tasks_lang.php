2ac8d987570afee928dea440852206adf0f4a726
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 23:39:31 +0000

    Add new files and directories and commit changes to local repository

--- language/ko/tasks_lang.php
+++ language/ko/tasks_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['tasks_app_description'] = 'The Task Scheduler can be used to run batch jobs on your system at regular time intervals.';
-$lang['tasks_app_name'] = 'Task Scheduler';
-$lang['tasks_schedule_configuration_already_exists'] = 'Schedule configuration already exists.';
+$lang['tasks_app_description'] = '작업 스케줄러는 일정한 시간 간격으로 시스템에 일괄 처리 작업을 실행하는 데 사용할 수 있습니다.';
+$lang['tasks_app_name'] = '작업 스케줄러';
+$lang['tasks_schedule_configuration_already_exists'] = '일정 구성이 이미 존재합니다.';
