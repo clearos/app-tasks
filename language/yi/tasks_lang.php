26cfd0f9ea1f9423258f2b34c86bea435d24decc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 21:06:54 +0000

    Add new files and directories and commit changes to local repository

--- language/yi/tasks_lang.php
+++ language/yi/tasks_lang.php
@@ -1,5 +1,5 @@
 <?php
 
-$lang['tasks_app_description'] = 'The Task Scheduler can be used to run batch jobs on your system at regular time intervals.';
-$lang['tasks_app_name'] = 'Task Scheduler';
-$lang['tasks_schedule_configuration_already_exists'] = 'Schedule configuration already exists.';
+$lang['tasks_app_description'] = 'די טאַסק סטשעדולער קענען ווערן געניצט צו לויפן פּעקל דזשאָבס אויף אייער סיסטעם בייַ רעגולער צייַט ינטערוואַלז.';
+$lang['tasks_app_name'] = 'טאַסק סטשעדולער';
+$lang['tasks_schedule_configuration_already_exists'] = 'פּלאַן קאָנפיגוראַטיאָן עקזיסטירט שוין.';
